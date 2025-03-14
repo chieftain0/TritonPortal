c9e3f6f1bc7b231706ef5e9f19137c2aa1ff8a4d
chieftain0 <100506519+chieftain0@users.noreply.github.com>
2025-03-14 13:55:16 +0400

    added manual mode

--- TritonPortal.ino
+++ TritonPortal.ino
@@ -13,13 +13,14 @@ Servo ESC3;
 Servo ESC4;
 
 const uint8_t ch1 = 13, ch2 = 12;
-const uint8_t SDA_pin = 4, SCL_pin = 5;
 const uint8_t ledPin = 21;
 uint8_t ledBrightness = 128;
 
 const uint8_t ESCPins[4] = {6, 7, 15, 16};
 
 volatile unsigned long highTime1 = 1500, highTime2 = 1500;
+
+volatile bool manualMode = true;
 
 void IRAM_ATTR pulseCh1()
 {
@@ -71,13 +72,14 @@ void setup()
   server.on("/ESC", handleESC);
   server.on("/data", handleData);
   server.on("/led", handleLed);
+  server.on("/switch", handleSwitch);
 
   xTaskCreatePinnedToCore(
-      webServerTask,   // Function to implement the task
-      "WebServerTask", // Name of the task
+      webServerTask,   // Task function
+      "WebServerTask", // Task name
       8192,            // Stack size
-      NULL,            // Parameters to the task
-      1,               // Task priority
+      NULL,            // Parameter
+      1,               // Priority
       NULL,            // Task handle
       0                // Core 0
   );
@@ -94,6 +96,13 @@ void webServerTask(void *pvParameters)
 
 void loop()
 {
+  if (!manualMode)
+  {
+    ESC1.writeMicroseconds(highTime1);
+    ESC2.writeMicroseconds(highTime1);
+    ESC3.writeMicroseconds(highTime2);
+    ESC4.writeMicroseconds(highTime2);
+  }
 }
 
 void handleRoot()
@@ -104,75 +113,84 @@ void handleRoot()
   html += "body { text-align: center; font-family: Arial, sans-serif; }";
   html += "#container { position: relative; width: 300px; height: 300px; border-radius: 50%; border: 2px solid black; margin: auto; }";
   html += "#ball { position: absolute; width: 20px; height: 20px; background: red; border-radius: 50%; }";
+  html += "input:disabled { background: #ddd; }";
   html += "</style>";
   html += "<script>";
   html += "var xhr = new XMLHttpRequest();";
-  html += "function updateESC(ESC, value){ xhr.open('GET', '/ESC?ESC=' + ESC + '&value=' + value, true); xhr.send(); }";
-  html += "function updateLED(value){ xhr.open('GET', '/led?value=' + value, true); xhr.send(); }";
-  html += "function updateData(){ xhr.open('GET', '/data', true); xhr.onreadystatechange = function(){";
-  html += "if(xhr.readyState == 4 && xhr.status == 200){";
-  html += "var data = JSON.parse(xhr.responseText);";
-  html += "document.getElementById('rc1').value = data.ch1;";
-  html += "document.getElementById('rc2').value = data.ch2;";
-  html += "document.getElementById('rc1Val').innerHTML = data.ch1;";
-  html += "document.getElementById('rc2Val').innerHTML = data.ch2;";
-
-  html += "var xInput = (data.ch1 - 1500) / 500;";
-  html += "var yInput = -1 * (data.ch2 - 1500) / 500;";
-
-  html += "var magnitude = Math.sqrt(xInput * xInput + yInput * yInput);";
-  html += "if (magnitude > 1) { xInput /= magnitude; yInput /= magnitude; }";
-
-  html += "var radius = 140;";
-  html += "var x = xInput * radius + 150 - 10;";
-  html += "var y = yInput * radius + 150 - 10;";
-
-  html += "document.getElementById('ball').style.left = x + 'px';";
-  html += "document.getElementById('ball').style.top = y + 'px';";
-
+  html += "function updateESC(ESC, value){";
+  html += "  xhr.open('GET', '/ESC?ESC=' + ESC + '&value=' + value, true);";
+  html += "  xhr.send();";
   html += "}";
-  html += "}; xhr.send(); }";
+  html += "function updateLED(value){";
+  html += "  xhr.open('GET', '/led?value=' + value, true);";
+  html += "  xhr.send();";
+  html += "}";
+  html += "function updateSwitch(checked){";
+  html += "  var state = checked ? 'on' : 'off';";
+  html += "  xhr.open('GET', '/switch?state=' + state, true);";
+  html += "  xhr.send();";
+  html += "  var sliders = document.getElementsByClassName('escSlider');";
+  html += "  for (var i = 0; i < sliders.length; i++) {";
+  html += "    sliders[i].disabled = !checked;";
+  html += "  }";
+  html += "}";
+  html += "function updateData(){";
+  html += "  xhr.open('GET', '/data', true);";
+  html += "  xhr.onreadystatechange = function(){";
+  html += "    if(xhr.readyState == 4 && xhr.status == 200){";
+  html += "      var data = JSON.parse(xhr.responseText);";
+  html += "      document.getElementById('rc1').value = data.ch1;";
+  html += "      document.getElementById('rc2').value = data.ch2;";
+  html += "      document.getElementById('rc1Val').innerHTML = data.ch1;";
+  html += "      document.getElementById('rc2Val').innerHTML = data.ch2;";
+  html += "      var xInput = (data.ch1 - 1500) / 500;";
+  html += "      var yInput = -1 * (data.ch2 - 1500) / 500;";
+  html += "      var magnitude = Math.sqrt(xInput*xInput + yInput*yInput);";
+  html += "      if (magnitude > 1) { xInput /= magnitude; yInput /= magnitude; }";
+  html += "      var radius = 140;";
+  html += "      var x = xInput * radius + 150 - 10;";
+  html += "      var y = yInput * radius + 150 - 10;";
+  html += "      document.getElementById('ball').style.left = x + 'px';";
+  html += "      document.getElementById('ball').style.top = y + 'px';";
+  html += "    }";
+  html += "  };";
+  html += "  xhr.send();";
+  html += "}";
   html += "setInterval(updateData, 100);";
   html += "</script></head><body>";
   html += "<h1>Triton Portal</h1>";
   html += "<h2>ESC Control</h2>";
+  html += "<label for='manualControl'>Manual Control: </label>";
+  html += "<input type='checkbox' id='manualControl' onchange='updateSwitch(this.checked)' /> <small>(Unchecked: RC input mode)</small>";
+  html += "<br><br>";
   for (int i = 1; i <= 4; i++)
   {
     html += "ESC " + String(i) + ": ";
-    html += "<input type='range' min='1000' max='2000' value='1500' oninput='updateESC(" + String(i) + ", this.value)' />";
+    html += "<input type='range' class='escSlider' min='1000' max='2000' value='1500' disabled oninput='updateESC(" + String(i) + ", this.value)' />";
     html += "<br><br>";
   }
   html += "<h2>LED Control</h2>";
   html += "LED Brightness: <input type='range' min='0' max='255' value='" + String(ledBrightness) + "' oninput='updateLED(this.value)' />";
   html += "<br><br>";
   html += "<h2>RC Channel Outputs</h2>";
-  html += "RC Channel 1: <input type='range' id='rc1' min='1000' max='2000' value='1500' disabled />";
-  html += " <span id='rc1Val'>1500</span><br><br>";
-  html += "RC Channel 2: <input type='range' id='rc2' min='1000' max='2000' value='1500' disabled />";
-  html += " <span id='rc2Val'>1500</span><br><br>";
+  html += "RC Channel 1: <input type='range' id='rc1' min='1000' max='2000' value='1500' disabled /> <span id='rc1Val'>1500</span><br><br>";
+  html += "RC Channel 2: <input type='range' id='rc2' min='1000' max='2000' value='1500' disabled /> <span id='rc2Val'>1500</span><br><br>";
   html += "<div id='container'><div id='ball'></div></div>";
   html += "</body></html>";
+
   server.send(200, "text/html", html);
 }
 
 void handleData()
 {
   if (highTime1 < 1000)
-  {
     highTime1 = 1000;
-  }
   else if (highTime1 > 2000)
-  {
     highTime1 = 2000;
-  }
   if (highTime2 < 1000)
-  {
     highTime2 = 1000;
-  }
   else if (highTime2 > 2000)
-  {
     highTime2 = 2000;
-  }
   String json = "{\"ch1\":" + String(highTime1) + ",\"ch2\":" + String(highTime2) + "}";
   server.send(200, "application/json", json);
 }
@@ -221,5 +239,29 @@ void handleLed()
   }
   ledBrightness = server.arg("value").toInt();
   ledcWrite(ledPin, ledBrightness);
+  server.send(200, "text/plain", "OK");
+}
+
+void handleSwitch()
+{
+  if (!server.hasArg("state"))
+  {
+    server.send(400, "text/plain", "Bad Request");
+    return;
+  }
+  String state = server.arg("state");
+  if (state == "off")
+  {
+    manualMode = false;
+  }
+  else if (state == "on")
+  {
+    manualMode = true;
+  }
+  else
+  {
+    server.send(400, "text/plain", "Invalid state");
+    return;
+  }
   server.send(200, "text/plain", "OK");
 }
